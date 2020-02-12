f0bb6f9bb2c7169aa69ef22e2922bb0ce8606fa6
janellep <janellepiedrahita@mec.science>
2020-02-11 20:08:47 -0500

    Febuary11

---
+++ Labs/lab01.cpp
@@ -0,0 +1,45 @@
+/*
+  Date: Feb 11
+  Assignment: In-class Lab01
+  Name: <Your First and Last Name>
+
+  To compile: g++ -o lab01 lab01.cpp
+  To execute: ./lab01
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // Todo:
+    // Task-0
+    int age;   
+    string firstname,lastname;
+
+    // Task-1
+firstname = "Frank";
+lastname = "Sinatra";
+age = 80;
+    
+    // Task-2
+cout << firstname <<"\n" <<lastname<<"\n" <<age<<"\n";
+
+    
+    // Task-3    
+    
+
+
+    // Task-4
+
+	
+
+    return 0;
+
+} // end of the main function
+
+
+
+
