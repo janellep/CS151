89dab28794c17c6c4b2a867df8ca214fe8bb04b3
janellep <janellepiedrahita@mec.science>
2020-02-21 14:38:27 -0500

    lab04 compelte

---
+++ Labs/lab04.cpp
@@ -0,0 +1,29 @@
+/*
+  Date: Feb 20
+  Assignment: In-class Lab04
+  Name: <Janelle  Piedrahita>
+
+  To compile: g++ -o lab04 lab04.cpp
+  To execute: ./lab04
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string fname ;
+    string lname ;
+    
+
+    
+    fname = "Janelle";
+    lname = "Piedrahita";
+    cout<<"*************\n"+lname+","+fname+"\n"+"*************\n"<<endl;
+
+
+
+    return 0;
+
+}
